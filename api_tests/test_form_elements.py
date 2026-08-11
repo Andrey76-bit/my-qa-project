09748183a81186dbb64ee2c51178335a92f4ffdc
Андрей <andrey@example.com>
2026-08-11 22:30:18 +0300

    Добавил POM-тест для чекбоксов

--- api_tests/test_form_elements.py
+++ api_tests/test_form_elements.py
@@ -1,4 +1,3 @@
-from pages.alerts_page import AlertsPage
 import pytest
 import os
 from selenium import webdriver
@@ -9,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from pages.alerts_page import AlertsPage
+from pages.checkboxes_page import CheckboxesPage
 
 @pytest.fixture
 def browser():
@@ -19,15 +19,13 @@ def browser():
     driver.quit()
 
 def test_checkboxes(browser):
-    browser.get("https://the-internet.herokuapp.com/checkboxes")
-    checkboxes = browser.find_elements(By.CSS_SELECTOR, "input[type='checkbox']")
-    first_checkbox = checkboxes[0]
-    second_checkbox = checkboxes[1]
-    assert not first_checkbox.is_selected()
-    assert second_checkbox.is_selected()
-    first_checkbox.click()
-    assert first_checkbox.is_selected()
-    print("✅ Чекбоксы пройдены")
+    page = CheckboxesPage(browser)
+    page.go_to()
+    assert not page.is_first_checkbox_selected(), "Первый чекбокс должен быть не отмечен"
+    assert page.is_second_checkbox_selected(), "Второй чекбокс должен быть отмечен"
+    page.click_first_checkbox()
+    assert page.is_first_checkbox_selected(), "Первый чекбокс должен стать отмеченным после клика"
+    print("✅ Чекбоксы пройдены (POM)")
 
 def test_dropdown(browser):
     browser.get("https://the-internet.herokuapp.com/dropdown")
@@ -51,11 +49,9 @@ def test_alert(browser):
     alerts = AlertsPage(browser)
     alerts.go_to()
     alerts.click_alert_button()
-
     alert = WebDriverWait(browser, 10).until(EC.alert_is_present())
     assert alert.text == "I am a JS Alert"
     alert.accept()
-
     assert "You successfully clicked an alert" in alerts.get_result_text()
     print("✅ Простой алерт пройден (POM)")
 
@@ -63,10 +59,8 @@ def test_confirm_alert(browser):
     alerts = AlertsPage(browser)
     alerts.go_to()
     alerts.click_confirm_button()
-
     alert = WebDriverWait(browser, 10).until(EC.alert_is_present())
     alert.dismiss()
-
     assert "You clicked: Cancel" in alerts.get_result_text()
     print("✅ Confirm алерт пройден (POM)")
 
@@ -74,10 +68,8 @@ def test_prompt_alert(browser):
     alerts = AlertsPage(browser)
     alerts.go_to()
     alerts.click_prompt_button()
-
     alert = WebDriverWait(browser, 10).until(EC.alert_is_present())
     alert.send_keys("Брат Андрей")
     alert.accept()
-
     assert "You entered: Брат Андрей" in alerts.get_result_text()
     print("✅ Prompt алерт пройден (POM)")
